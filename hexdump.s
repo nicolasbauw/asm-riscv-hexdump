2dbf167aafc986bdc6440794518d7e7517f4ea44
Nicolas BAUW <nbauw@hotmail.com>
2024-02-19 20:21:05 +0100

    Printing the whole file content

--- hexdump.s
+++ hexdump.s
@@ -1,5 +1,8 @@
 .global _start
 .global line_buffer
+
+.equ BUF_SIZE,4096
+
 .text
 
 _start:
@@ -16,7 +19,7 @@ _start:
     ecall
     blt     a0,x0,exit          # Error ? we exit
 
-    mv      t5,a0               # we save the file descriptor in t5
+    mv      s7,a0               # we save the file descriptor in s7
     li      a1,0                # offset
     li      a2,2                # SEEK_END
     li      a7,62               # "lseek" system call
@@ -24,24 +27,34 @@ _start:
     blt     a0,x0,close_exit    # Error ? we close the FD and exit
     mv      t6,a0               # we save the file size in t6
 
-    mv      a0,t5               # first argument : the file descriptor (saved in t5)
+    mv      a0,s7               # first argument : the file descriptor (saved in s7)
     li      a1,0                # offset
     li      a2,0                # SEEK_SET
     li      a7,62               # "lseek" system call
     ecall
     blt     a0,x0,close_exit    # Error ? we close the FD and exit
 
-    mv      a0,t5               # Opened FD
+hexdump:
+    mv      a0,s7               # Opened FD
     la      a1,buffer
-    li      a2,64               # we read 64 bytes and store them to the buffer
+    li      a2,BUF_SIZE         # we read 4096 bytes and store them to the buffer
     li      a7,63               # "read" system call
     ecall
+    mv      t4,a0               # Saving number of read bytes to t4
 
-    la      a0,buffer
-    jal     print_line          # Print test line
+    la      t5,buffer           # t5 = Pointer to the bytes buffer
+print_all_lines:
+    mv      a0,t5               # a0 = argument for print_lines function
+    jal     print_line          # Prints a 16 bytes line
+    addi    t4,t4,-16           # Decrementing number of bytes to process
+    blt     t4,x0,close_exit    # EOF ? close FD and exit
+    beq     t4,x0,hexdump       # End of buffer ? We read the next 4096 bytes
+    
+    addi    t5,t5,16            # Preparing buffer pointer for next 16 bytes
+    j       print_all_lines     # Continue processing lines until the end of the 4096-bytes buffer
 
 close_exit:
-    mv      a0,t5               # file descriptor was saved in t5
+    mv      a0,s7               # file descriptor was saved in s7
     li      a7,57               # "close" system call
     ecall
 
@@ -51,4 +64,4 @@ exit:
 
 
 .lcomm line_buffer,49
-.lcomm buffer,64
+.lcomm buffer,BUF_SIZE
